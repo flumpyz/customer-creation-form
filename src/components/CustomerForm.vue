e61f342f7a8e39a8ea86cc71acd4436b3e2c1bef
flumpyz <natalianata2001@mail.ru>
2022-04-09 18:12:40 +0500

    added fields to the form

--- src/components/CustomerForm.vue
+++ src/components/CustomerForm.vue
@@ -1,8 +1,8 @@
 <template>
   <form class="create-form">
-    <CustomerFormInputField label-title="Фамилия" is-field-required />
-    <CustomerFormInputField label-title="Имя" is-field-required />
-    <CustomerFormInputField label-title="Отчество" v-bind:is-field-required="false" />
+    <CustomerFormInputField label-title="Фамилия" is-field-required/>
+    <CustomerFormInputField label-title="Имя" is-field-required/>
+    <CustomerFormInputField label-title="Отчество" v-bind:is-field-required="false"/>
 
     <label for="birth-date-input">
       Дата рождения <span class="required-field-sign">*</span>
@@ -15,25 +15,66 @@
     <input type="tel" id="phone-number-input" class="input-form">
 
     <div>
-      <input type="radio" id="female-gender-selection-radio" name="gender-selection-radio" value="Женский" >
+      <input type="radio" id="female-gender-selection-radio" name="gender-selection-radio" value="Женский">
       <label for="female-gender-selection-radio">Женский</label>
     </div>
 
     <div>
-      <input type="radio" id="male-gender-selection-radio" name="gender-selection-radio" value="Мужской" >
+      <input type="radio" id="male-gender-selection-radio" name="gender-selection-radio" value="Мужской">
       <label for="male-gender-selection-radio">Мужской</label>
     </div>
 
+    <MultiSelect v-bind:options="options"/>
+
+    <select>
+      <option>Иванов</option>
+      <option>Захаров</option>
+      <option>Чернышева</option>
+    </select>
+
+    <div>
+      <input type="checkbox" id="is-send-messages-input">
+      <label for="is-send-messages-input">Не отправлять СМС</label>
+    </div>
+
+    <CustomerFormInputField label-title="Индекс" v-bind:is-field-required="false"/>
+    <CustomerFormInputField label-title="Страна" v-bind:is-field-required="false"/>
+    <CustomerFormInputField label-title="Область" v-bind:is-field-required="false"/>
+    <CustomerFormInputField label-title="Город" is-field-required/>
+    <CustomerFormInputField label-title="Улица" v-bind:is-field-required="false"/>
+    <CustomerFormInputField label-title="Дом" v-bind:is-field-required="false"/>
+
+    <select>
+      <option>Паспорт</option>
+      <option>Свидетельство о рождении</option>
+      <option>Вод. удостоверение</option>
+    </select>
+
+    <CustomerFormInputField label-title="Серия" v-bind:is-field-required="false"/>
+    <CustomerFormInputField label-title="Номер" v-bind:is-field-required="false"/>
+    <CustomerFormInputField label-title="Кем выдан" v-bind:is-field-required="false"/>
+    <CustomerFormInputField label-title="Дата выдачи" is-field-required/>
   </form>
 </template>
 
 <script>
 import CustomerFormInputField from "@/components/CustomerFormInputField";
+import MultiSelect from "@/components/MultiSelect";
 
 export default {
   name: "CustomerForm",
   components: {
+    MultiSelect,
     CustomerFormInputField
+  },
+  data() {
+    return {
+      options: [
+        {title: "VIP"},
+        {title: "Проблемные"},
+        {title: "ОМС"},
+      ]
+    }
   }
 }
 </script>
---
+++ src/components/MultiSelect.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div>
+      Выберите группу клиентов:
+    </div>
+
+    <div class="multi-select-options">
+      <div class="multi-select-option"
+           v-for="(option, index) in options"
+           :key="index">
+        <input type="checkbox" id="option-checkbox">
+        <label for="option-checkbox">{{ option.title }}</label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MultiSelect",
+  props: {
+    options: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
